ca0f810788b374adc2dbef64d72bf4c433721b71
Caleb Kopp <c.kopp.professional@gmail.com>
2019-11-21 11:36:50 -0600

    initial commit of google-able tasks

---
+++ part2/xxlab12-2.py
@@ -0,0 +1,10 @@
+# Take in a file name from the user
+# Open the file specified by the user
+# Read all the text from that file into some sort of container
+#   either a list, or one big string
+# Take each element of this container and as a Byte
+#   add 4 to the Byte
+#   store in a different container (either list or string)
+# Ask the user for an output filename
+# Write out the string data from that new container to
+#   the output file
